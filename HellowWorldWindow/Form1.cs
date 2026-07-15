7440936fdaf00b5dd1a50de50f9628abd460d4ad
Cavan Theiss <cavanrt@gmail.com>
2026-07-15 15:33:55 -0500

    Update Form1.cs

--- HellowWorldWindow/Form1.cs
+++ HellowWorldWindow/Form1.cs
@@ -1,1 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace HelloWorldWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "I'm Blue!";
+            lblGoodbyeWorld.Text = "Click Here!";
+            this.BackColor = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblGoodbyeWorld.Text = "I'm Red!";
+            lblHelloWorld.Text = "Click Here!";
+            this.BackColor = Color.Red;
+        }
+
+    }
+}
